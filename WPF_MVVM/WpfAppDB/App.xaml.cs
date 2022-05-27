ba363469ae5033800b9470dc1977b8b119f2fe52
Andrey-2021 <job2017ya@yandex.ru>
2022-05-27 22:46:22 +0300

    небольшие изменения

--- WPF_MVVM/WpfAppDB/App.xaml.cs
+++ WPF_MVVM/WpfAppDB/App.xaml.cs
@@ -1,12 +1,5 @@
 ﻿using Serilog;
 using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.IO;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using WpfAppDB.Classes;
 using WpfAppDB.Model;
@@ -35,11 +28,11 @@ namespace WpfAppDB
             //Регистрируем Пары Типов View и ViewModel в контроллере 
             WindowsController.GetInstance().RegistryWindowType<MainWindowViewModel, MainWindowView>();
             WindowsController.GetInstance().RegistryWindowType<ProductWindowViewModel, ProductWindowView>();
-            WindowsController.GetInstance().RegistryWindowType<ShopWindowViewModel,ShopWindowView>();
+            WindowsController.GetInstance().RegistryWindowType<ShopWindowViewModel, ShopWindowView>();
             WindowsController.GetInstance().RegistryWindowType<MessageWindowViewModel, MessageWindowView>();
         }
 
-        
+
         protected override void OnStartup(System.Windows.StartupEventArgs e)
         {
             base.OnStartup(e);
@@ -48,18 +41,18 @@ namespace WpfAppDB
                 Log.Information("Запуск программы. Метод OnStartup(), файл App.xaml.cs");
 
                 model = new DbModel(); //создали модель
-                
+
                 //используем Dependency injection через конструктор:
                 var mainViewModel = new MainWindowViewModel(model); //создаём ViewModel главного окна
 
                 WindowsController.GetInstance().ShowWindow(mainViewModel);  //показать окно, соответствующее mainViewModel
-                
+
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Log.Fatal(ex, "Неожиданная ошибка в программе. Метод OnSturtup()");
 
-                MessageWindowViewModel model = new MessageWindowViewModel("Ошибка выполнения программы: "+ex.Message);
+                MessageWindowViewModel model = new MessageWindowViewModel("Ошибка выполнения программы: " + ex.Message);
                 WindowsController.GetInstance().ShowWindow(model);
 
             }
@@ -81,8 +74,8 @@ namespace WpfAppDB
             Log.Logger = new LoggerConfiguration()
                     .MinimumLevel.Debug()
                     .Enrich.WithThreadId()   //Будем Добавлять в события Serilog Id потока.
-                    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day, fileSizeLimitBytes:200_000_000,
-                    outputTemplate:"{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {Properties}{NewLine}{Exception}")
+                    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day, fileSizeLimitBytes: 200_000_000,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj} {Properties}{NewLine}{Exception}")
                     .CreateLogger();
         }
     }
